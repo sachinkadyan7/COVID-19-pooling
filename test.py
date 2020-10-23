83e0bb996e43e2ed2c3d1da67328385295eaca0f
imyiningliu <lyining@berkeley.edu>
2020-10-22 22:50:54 -0400

    update random measurement matrix

--- test.py
+++ test.py
@@ -15,11 +15,15 @@ def recover_pool_results(membership_matrix, pool_results, fpr, fnr, f):
     _, num_trials = pool_results.shape
 
     recovered_xs = np.empty((num_samples, num_trials))
+    recovered_false_ps = np.empty((num_pools, num_trials))
+    recovered_false_ns = np.empty((num_pools, num_trials))
     for trial in range(num_trials):
         if trial % 10 == 0:
             print("Starting trail %s ..." % trial)
         pool_result = pool_results[:, trial]
-        solution, status = solve_mip(membership_matrix, pool_result, fpr, fnr, f)
-        recovered_xs[:, trial] = solution
+        recovered_x, recovered_false_p, recovered_false_n, status = solve_mip(membership_matrix, pool_result, fpr, fnr, f)
+        recovered_xs[:, trial] = recovered_x
+        recovered_false_ps[:, trial] = recovered_false_p
+        recovered_false_ns[:, trial] = recovered_false_n
 
-    return recovered_xs
+    return recovered_xs, recovered_false_ps, recovered_false_ns
