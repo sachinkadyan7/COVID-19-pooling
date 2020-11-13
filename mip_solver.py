86cc115037109109139c3043ff461059c819b08b
imyiningliu <lyining@berkeley.edu>
2020-11-13 09:55:54 -0500

    fix assert statement; cr sachin

--- mip_solver.py
+++ mip_solver.py
@@ -1,5 +1,7 @@
 from mip import *
 from util import check_inputs
+import scipy.io
+from numpy import genfromtxt
 
 
 def solve_mip(membership_matrix, pool_result, fpr, fnr, f):
@@ -25,8 +27,8 @@ def solve_mip(membership_matrix, pool_result, fpr, fnr, f):
     pool_false_positives = [m.add_var(name=str(i + num_samples), var_type=BINARY) for i in range(num_pools)]
     pool_false_negatives = [m.add_var(name=str(i + num_samples+num_pools), var_type=BINARY) for i in range(num_pools)]
 
-    pool_fn_prime = [m.add_var(var_type=INTEGER, lb=0) for i in range(num_pools)]
-    b = [m.add_var(var_type=BINARY) for i in range(num_pools)]
+    pool_fn_prime = [m.add_var(var_type=INTEGER, lb=0) for _ in range(num_pools)]
+    b = [m.add_var(var_type=BINARY) for _ in range(num_pools)]
 
     # Add constraints
     for i in range(num_pools):
@@ -61,7 +63,16 @@ def solve_mip(membership_matrix, pool_result, fpr, fnr, f):
             recovered_false_p[i] = m.vars[i + num_samples].x
             recovered_false_n[i] = m.vars[i + num_samples + num_pools].x
 
-    assert np.sgn(membership_matrix @ recovered_x) + recovered_false_p - recovered_false_n == pool_result
+    assert np.all(np.sign(membership_matrix @ recovered_x) + recovered_false_p - recovered_false_n == pool_result)
 
     return recovered_x, recovered_false_p, recovered_false_n
 
+
+matrix_file = scipy.io.loadmat('/Users/yiningliu/research/pooled-sampling/COVID-19-pooling/tests/data/shental-poolingMatrix.mat')
+membership_matrix = matrix_file['poolingMatrix']
+true_infection_vectors_file = '/Users/yiningliu/research/pooled-sampling/COVID-19-pooling/tests/data/x-f-1-384.csv'
+xs = genfromtxt(true_infection_vectors_file, delimiter=',')
+x = xs[:, 0]
+pool_result = np.sign(membership_matrix @ x)
+
+solve_mip(membership_matrix, pool_result, 0, 0, 1/384)
